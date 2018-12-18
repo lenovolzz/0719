04083f5badbd3941f1a1fb7e1b90ba59949eac8d
lenovolzz <1197288092@qq.com>
2018-12-18 10:50:26 +0800

    首次创建项目，并提交到本地仓库中！！！！！！！！！！！！！！！

---
+++ TestGit/src/cn/gzsxt/git/test/TestGit.java
@@ -0,0 +1,13 @@
+package cn.gzsxt.git.test;
+/**
+ *@Author: Lee
+ *@E-mail:1197288092@qq.com
+ *@QQ:1197288092
+ *@Tele:13610233670
+ *@Date:2018年12月18日---上午10:46:21
+ */
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("this is the first git repository!!!!!!!!");
+	}
+}
